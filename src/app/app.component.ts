81cc180a8d0fa6e19d5497bad6776ace5805b6e8
Alma Babic <alma.babic@tacta.io>
2024-12-25 14:19:34 +0100

    Section 8 - Lecture 163

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,28 @@
+import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
+  selector: 'app-root',   // This defines the custom HTML tag that will represent this component in the HTML. In this case, it’s <app-root>.
+  standalone: true,   // This indicates that the component is a standalone component and can be used independently (not part of a module). 
+  templateUrl: './app.component.html',   // This refers to the HTML template file (app.component.html) that defines the structure of the component's view (what the user sees on the screen).
+  imports: [DatePipe, DecimalPipe]   
+  // DatePipe is a built-in pipe that allows you to format dates. 
+  // DecimalPipe is a built-in pipe that is used to format numeric values.
 })
 export class AppComponent {
-  currentDate = new Date();
-  currentTemperaturs = {
+  currentDate = new Date();  // This initializes the currentDate property with the current date and time using JavaScript's Date object.
+  currentTemperaturs = {  // This object holds the current temperature for several cities.
     berlin: 4.2749812,
     newYork: 18.1214,
     paris: 72.1209001,
     chicago: 65.0775238,
   };
 
-  historicTemperatures = [
+  historicTemperatures = [  // This is an array that represents a list of temperatures for different time points. These might represent historical temperature data, such as from previous days or months.
     25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
   ];
 
-  onReset(index: number) {
+  onReset(index: number) {  // This method accepts an index and resets the temperature at that index in the historicTemperatures array to 18.
     this.historicTemperatures[index] = 18;
   }
 }
